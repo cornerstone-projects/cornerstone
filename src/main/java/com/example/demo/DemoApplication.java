962a5ba853cfd330f13eea3cb2ef4f25c9cb0717
Yanming Zhou <zhouyanming@gmail.com>
2021-06-18 10:05:40 +0800

    Active profile "dev" if devtools present

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.util.ClassUtils;
 
 import com.zaxxer.hikari.HikariDataSource;
 
@@ -20,6 +22,16 @@ public class DemoApplication implements CommandLineRunner {
 	private ApplicationContext applicationContext;
 
 	public static void main(String[] args) {
+		if (ClassUtils.isPresent("org.springframework.boot.devtools.RemoteSpringApplication",
+				DemoApplication.class.getClassLoader())) {
+			String profiles = System.getProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME);
+			if (profiles == null) {
+				profiles = System
+						.getenv(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME.replaceAll("\\.", "_").toUpperCase());
+				if (profiles == null)
+					System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "dev");
+			}
+		}
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
