c06521ec5abc7d7857ae9c51eb3ee53ff016157b
Yanming Zhou <zhouyanming@gmail.com>
2021-07-05 17:05:36 +0800

    Logging rest request/response

--- src/main/java/com/example/demo/core/servlet/ServletConfiguration.java
+++ src/main/java/com/example/demo/core/servlet/ServletConfiguration.java
@@ -1,5 +1,9 @@
 package com.example.demo.core.servlet;
 
+import java.util.List;
+
+import javax.servlet.DispatcherType;
+
 import org.slf4j.MDC;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -11,10 +15,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 
 @Configuration(proxyBeanMethods = false)
 @Profile("!test")
 public class ServletConfiguration {
+
+	@Bean
+	RestFilter restFilter() {
+		return new RestFilter();
+	}
+
+	@Bean
+	FilterRegistrationBean<RestFilter> restFilterRegistration(RestFilter restFilter) {
+		FilterRegistrationBean<RestFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(restFilter);
+		registrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR, DispatcherType.ASYNC);
+		registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 52);
+		// after AccessFilter
+		return registrationBean;
+	}
 
 	@Bean
 	AccessFilter accessFilter() {
@@ -29,11 +49,11 @@ public class ServletConfiguration {
 		// after SessionRepositoryFilter.DEFAULT_ORDER
 		return registrationBean;
 	}
-	
+
 	@Bean
 	@ConditionalOnProperty(prefix = "portal", name = "baseUrl")
 	SsoFilter ssoFilter(RestTemplateBuilder builder, UserDetailsService userDetailsService) {
-		return new SsoFilter( builder,  userDetailsService);
+		return new SsoFilter(builder, userDetailsService);
 	}
 
 	@Bean
@@ -46,14 +66,17 @@ public class ServletConfiguration {
 	}
 
 	@Bean
-	RestTemplateCustomizer requestIdRestTemplateCustomizer() {
-		return restTemplate -> restTemplate.getInterceptors().add((request, body, execution) -> {
-			String requestId = MDC.get(AccessFilter.MDC_KEY_REQUEST_ID);
-			if (requestId != null)
-				request.getHeaders().set(AccessFilter.HTTP_HEADER_REQUEST_ID, requestId);
-			return execution.execute(request, body);
-		});
-
+	RestTemplateCustomizer myRestTemplateCustomizer() {
+		return restTemplate -> {
+			List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+			interceptors.add((request, body, execution) -> {
+				String requestId = MDC.get(AccessFilter.MDC_KEY_REQUEST_ID);
+				if (requestId != null)
+					request.getHeaders().set(AccessFilter.HTTP_HEADER_REQUEST_ID, requestId);
+				return execution.execute(request, body);
+			});
+			interceptors.add(new LoggingClientHttpRequestInterceptor());
+		};
 	}
 
 }
