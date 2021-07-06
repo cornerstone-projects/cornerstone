c1a79d3b47cd3ce1a7289d7726cd241aad682a94
Yanming Zhou <zhouyanming@gmail.com>
2021-07-06 15:42:28 +0800

    Use i18n

---
+++ src/main/java/com/example/demo/Messages.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public interface Messages {
+	// mapping to messages.properties
+
+	String NOT_FOUND = "not.found";
+}
--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -1,12 +1,14 @@
 package com.example.demo.user;
 
 import static com.example.demo.MainApplication.ADMIN_ROLE;
+import static com.example.demo.Messages.NOT_FOUND;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -33,6 +35,9 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private MessageSource messageSource;
+
 	@GetMapping("/users")
 	public ResultPage<User> list(@Min(1) @RequestParam(required = false, defaultValue = "1") int pageNo,
 			@Min(10) @Max(100) @RequestParam(required = false, defaultValue = "10") int pageSize,
@@ -52,8 +57,8 @@ public class UserController {
 
 	@GetMapping("/user/{id}")
 	public User get(@Min(1) @PathVariable Long id) {
-		return userRepository.findById(id)
-				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "ID not found: " + id));
+		return userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+				messageSource.getMessage(NOT_FOUND, new Object[] { id }, null)));
 	}
 
 	@PostMapping("/users")
@@ -68,15 +73,17 @@ public class UserController {
 		userRepository.findById(id).map(u -> {
 			BeanUtils.copyProperties(user, u, "id", "username", "password");
 			return userRepository.save(u);
-		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "ID not found: " + id));
+		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+				messageSource.getMessage(NOT_FOUND, new Object[] { id }, null)));
 	}
 
 	@DeleteMapping("/user/{id}")
 	@PreAuthorize("hasRole('" + ADMIN_ROLE + "')")
 	public void delete(@Min(1) @PathVariable Long id) {
 		// do not use deleteById, not annotated by @Cacheable
-		userRepository.delete(userRepository.findById(id)
-				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "ID not found: " + id)));
+		userRepository
+				.delete(userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+						messageSource.getMessage(NOT_FOUND, new Object[] { id }, null))));
 	}
 
 }
