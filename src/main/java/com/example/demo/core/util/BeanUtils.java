f84dc4f2c0be296386163739f9b76ce506a25888
Yanming Zhou <zhouyanming@gmail.com>
2021-07-12 15:54:09 +0800

    Introduce BeanUtils::copyPropertiesInJsonView

--- src/main/java/com/example/demo/core/util/BeanUtils.java
+++ src/main/java/com/example/demo/core/util/BeanUtils.java
@@ -1,6 +1,8 @@
 package com.example.demo.core.util;
 
 import java.beans.FeatureDescriptor;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -9,6 +11,8 @@ import java.util.stream.Stream;
 
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
+
+import com.fasterxml.jackson.annotation.JsonView;
 
 import lombok.experimental.UtilityClass;
 
@@ -24,4 +28,31 @@ public class BeanUtils {
 		org.springframework.beans.BeanUtils.copyProperties(source, target, ignores.toArray(new String[ignores.size()]));
 	}
 
+	public static void copyPropertiesInJsonView(Object source, Object target, Class<?> view) {
+		BeanWrapper sourceBW = new BeanWrapperImpl(source);
+		BeanWrapper targetBW = new BeanWrapperImpl(target);
+		for (PropertyDescriptor pd : sourceBW.getPropertyDescriptors()) {
+			String name = pd.getName();
+			Method m = pd.getReadMethod();
+			if (m == null)
+				continue;
+			JsonView jsonView = m.getAnnotation(JsonView.class);
+			if (jsonView == null) {
+				try {
+					jsonView = m.getDeclaringClass().getDeclaredField(name).getAnnotation(JsonView.class);
+				} catch (Exception e) {
+				}
+			}
+			if (jsonView == null)
+				continue;
+			for (Class<?> clazz : jsonView.value()) {
+				if (clazz.isAssignableFrom(view)) {
+					targetBW.setPropertyValue(name, sourceBW.getPropertyValue(name));
+					break;
+				}
+			}
+
+		}
+
+	}
 }
--- src/test/java/com/example/demo/core/util/BeanUtilsTests.java
+++ src/test/java/com/example/demo/core/util/BeanUtilsTests.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
+import com.fasterxml.jackson.annotation.JsonView;
+
 import lombok.Data;
 
 public class BeanUtilsTests {
@@ -13,11 +15,23 @@ public class BeanUtilsTests {
 
 		private Integer id;
 
+		@JsonView(View.A.class)
 		private String username;
 
+		@JsonView(View.B.class)
 		private String password;
 
 		private Boolean enabled;
+
+		interface View {
+			interface A {
+
+			}
+
+			interface B extends A {
+
+			}
+		}
 	}
 
 	@Test
@@ -42,6 +56,32 @@ public class BeanUtilsTests {
 		assertThat(user3.getId()).isNull();
 		assertThat(user3.getEnabled()).isNull();
 		assertThat(user2.getUsername()).isEqualTo(user1.getUsername());
+	}
+
+	@Test
+	public void copyPropertiesInJsonView() {
+		User user1 = new User();
+		user1.setId(1);
+		user1.setUsername("username1");
+		user1.setPassword("password1");
+		user1.setEnabled(false);
+
+		User user2 = new User();
+		user2.setEnabled(true);
+		BeanUtils.copyPropertiesInJsonView(user1, user2, User.View.A.class);
+		assertThat(user2.getId()).isNull();
+		assertThat(user2.getUsername()).isEqualTo(user1.getUsername());
+		assertThat(user2.getPassword()).isNull();
+		assertThat(user2.getEnabled()).isEqualTo(Boolean.TRUE);
+
+		User user3 = new User();
+		user3.setEnabled(true);
+		BeanUtils.copyPropertiesInJsonView(user1, user3, User.View.B.class);
+		assertThat(user3.getId()).isNull();
+		assertThat(user3.getUsername()).isEqualTo(user1.getUsername());
+		assertThat(user3.getPassword()).isEqualTo(user1.getPassword());
+		assertThat(user3.getEnabled()).isEqualTo(Boolean.TRUE);
+
 	}
 
 }
