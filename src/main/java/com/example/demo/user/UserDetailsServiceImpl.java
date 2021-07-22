7a8c159bdd79b218374fe5e9252c1a4c06ef9b1c
Yanming Zhou <zhouyanming@gmail.com>
2021-07-22 10:49:39 +0800

    Implement UserDetailsPasswordService

--- src/main/java/com/example/demo/user/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/user/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.demo.user;
 
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
@@ -9,12 +10,21 @@ import lombok.RequiredArgsConstructor;
 
 @Component("userDetailsService")
 @RequiredArgsConstructor
-public class UserDetailsServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService, UserDetailsPasswordService {
 
 	private final UserRepository userRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+	}
+
+	@Override
+	public UserDetails updatePassword(UserDetails user, String newPassword) {
+		String username = user.getUsername();
+		User u = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+		u.setPassword(newPassword);
+		userRepository.save(u);
+		return u;
 	}
 }
