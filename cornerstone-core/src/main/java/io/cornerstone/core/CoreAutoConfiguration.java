a1b9be738367d1a0054e2ff5255141bb7731a5d0
Yanming Zhou <zhouyanming@gmail.com>
2021-08-04 15:33:38 +0800

    Remove unnecessary @EnableJpaRepositories

--- cornerstone-core/src/main/java/io/cornerstone/core/CoreAutoConfiguration.java
+++ cornerstone-core/src/main/java/io/cornerstone/core/CoreAutoConfiguration.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration(proxyBeanMethods = false)
 @ComponentScan
 @EntityScan
-@EnableJpaRepositories
 @AutoConfigureBefore(TaskExecutionAutoConfiguration.class)
 public class CoreAutoConfiguration {
 
