dddf5e10f88273068696dabb6aa4e5c600273f83
Yanming Zhou <zhouyanming@gmail.com>
2021-08-12 09:15:05 +0800

    Swallow QueryTimeoutException for cache

--- cornerstone-core/src/main/java/io/cornerstone/core/cache/CacheConfiguration.java
+++ cornerstone-core/src/main/java/io/cornerstone/core/cache/CacheConfiguration.java
@@ -1,12 +1,19 @@
 package io.cornerstone.core.cache;
 
+import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Profile("!test")
 @EnableCaching
 @Configuration(proxyBeanMethods = false)
-public class CacheConfiguration {
+public class CacheConfiguration extends CachingConfigurerSupport {
+
+	@Override
+	public CacheErrorHandler errorHandler() {
+		return new DefaultCacheErrorHandler();
+	}
 
 }
---
+++ cornerstone-core/src/main/java/io/cornerstone/core/cache/DefaultCacheErrorHandler.java
@@ -0,0 +1,49 @@
+package io.cornerstone.core.cache;
+
+import org.springframework.cache.Cache;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.dao.QueryTimeoutException;
+import org.springframework.lang.Nullable;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DefaultCacheErrorHandler implements CacheErrorHandler {
+
+	@Override
+	public void handleCacheGetError(RuntimeException exception, Cache cache, Object key) {
+		if (exception instanceof QueryTimeoutException) {
+			log.error(exception.getMessage());
+			return;
+		}
+		throw exception;
+	}
+
+	@Override
+	public void handleCachePutError(RuntimeException exception, Cache cache, Object key, @Nullable Object value) {
+		if (exception instanceof QueryTimeoutException) {
+			log.error(exception.getMessage());
+			return;
+		}
+		throw exception;
+	}
+
+	@Override
+	public void handleCacheEvictError(RuntimeException exception, Cache cache, Object key) {
+		if (exception instanceof QueryTimeoutException) {
+			log.error(exception.getMessage());
+			return;
+		}
+		throw exception;
+	}
+
+	@Override
+	public void handleCacheClearError(RuntimeException exception, Cache cache) {
+		if (exception instanceof QueryTimeoutException) {
+			log.error(exception.getMessage());
+			return;
+		}
+		throw exception;
+	}
+
+}
