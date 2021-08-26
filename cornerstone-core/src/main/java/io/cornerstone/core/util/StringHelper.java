d229580b9cef68ab831cf015bbd14395147a7e8d
Yanming Zhou <zhouyanming@gmail.com>
2021-08-26 17:36:05 +0800

    Add StringHelper

---
+++ cornerstone-core/src/main/java/io/cornerstone/core/util/StringHelper.java
@@ -0,0 +1,95 @@
+package io.cornerstone.core.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.util.StringUtils;
+
+public class StringHelper {
+
+	public static String pluralOf(String string) {
+
+		if (!StringUtils.hasLength(string) || string.endsWith("ses") || string.endsWith("ies"))
+			return string;
+
+		int index = -1;
+		boolean underscore = string.indexOf('_') > 0;
+		if (underscore) {
+			index = string.lastIndexOf('_') + 1;
+		} else {
+			// camel case
+			for (int i = 0; i < string.length(); i++) {
+				if (Character.isUpperCase(string.charAt(i))) {
+					index = i;
+				}
+			}
+		}
+		String prefix;
+		String word;
+		if (index > 0) {
+			prefix = string.substring(0, index);
+			word = string.substring(index).toLowerCase();
+		} else {
+			prefix = "";
+			word = string;
+		}
+		if (specialCases.containsValue(word))
+			return string;
+		String plural = specialCases.get(word);
+		if (plural == null) {
+			int length = word.length();
+			char lastLetter = word.charAt(length - 1);
+			char secondLast = word.charAt(length - 2);
+			if ("sxzo".indexOf(lastLetter) >= 0 || (lastLetter == 'h' && (secondLast == 's' || secondLast == 'c'))) {
+				plural = word + "es";
+			} else if (lastLetter == 'y') {
+				if ("aeiou".indexOf(secondLast) >= 0) {
+					plural = word + "s";
+				} else {
+					plural = word.substring(0, length - 1) + "ies";
+				}
+			} else {
+				plural = word + "s";
+			}
+		}
+		return prefix + (underscore ? plural : prefix.isEmpty() ? plural : StringUtils.capitalize(plural));
+	}
+
+	private static Map<String, String> specialCases = new HashMap<>();
+	static {
+		specialCases.put("alumnus", "alumni");
+		specialCases.put("bison", "bison");
+		specialCases.put("child", "children");
+		specialCases.put("datum", "data");
+		specialCases.put("elf", "elves");
+		specialCases.put("elk", "elk");
+		specialCases.put("fish", "fish");
+		specialCases.put("foot", "feet");
+		specialCases.put("gentleman", "gentlemen");
+		specialCases.put("gentlewoman", "gentlewomen");
+		specialCases.put("goose", "geese");
+		specialCases.put("grouse", "grouse");
+		specialCases.put("half", "halves");
+		specialCases.put("knife", "knives");
+		specialCases.put("leaf", "leaves");
+		specialCases.put("life", "lives");
+		specialCases.put("louse", "lice");
+		specialCases.put("man", "men");
+		specialCases.put("money", "monies");
+		specialCases.put("moose", "moose");
+		specialCases.put("mouse", "mice");
+		specialCases.put("ox", "oxen");
+		specialCases.put("self", "selves");
+		specialCases.put("sheaf", "sheaves");
+		specialCases.put("sheep", "sheep");
+		specialCases.put("shelf", "shelves");
+		specialCases.put("squid", "squid");
+		specialCases.put("thief", "thieves");
+		specialCases.put("tooth", "teeth");
+		specialCases.put("wharf", "wharves");
+		specialCases.put("wife", "wives");
+		specialCases.put("wolf", "wolves");
+		specialCases.put("woman", "women");
+	}
+
+}
---
+++ cornerstone-core/src/test/java/io/cornerstone/core/util/StringHelperTests.java
@@ -0,0 +1,28 @@
+package io.cornerstone.core.util;
+
+import static io.cornerstone.core.util.StringHelper.pluralOf;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class StringHelperTests {
+
+	@Test
+	public void testPluralOf() {
+		assertThat(pluralOf(null)).isNull();
+		assertThat(pluralOf("")).isEqualTo("");
+		assertThat(pluralOf("man")).isEqualTo("men");
+		assertThat(pluralOf("testMan")).isEqualTo("testMen");
+		assertThat(pluralOf("test_man")).isEqualTo("test_men");
+		assertThat(pluralOf("person")).isEqualTo("persons");
+		assertThat(pluralOf("testPerson")).isEqualTo("testPersons");
+		assertThat(pluralOf("test_person")).isEqualTo("test_persons");
+		assertThat(pluralOf("bus")).isEqualTo("buses");
+		assertThat(pluralOf("testBus")).isEqualTo("testBuses");
+		assertThat(pluralOf("test_bus")).isEqualTo("test_buses");
+		assertThat(pluralOf("entity")).isEqualTo("entities");
+		assertThat(pluralOf("testEntity")).isEqualTo("testEntities");
+		assertThat(pluralOf("test_entity")).isEqualTo("test_entities");
+	}
+
+}
