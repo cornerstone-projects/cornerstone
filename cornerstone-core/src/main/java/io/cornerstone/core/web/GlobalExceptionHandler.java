b32dc5b93e87885aea1eb3e045308b799f903d1f
Yanming Zhou <zhouyanming@gmail.com>
2021-08-30 18:04:37 +0800

    Use @ResponseStatus instead of HttpServletResponse::sendError

--- cornerstone-core/src/main/java/io/cornerstone/core/web/GlobalExceptionHandler.java
+++ cornerstone-core/src/main/java/io/cornerstone/core/web/GlobalExceptionHandler.java
@@ -8,8 +8,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import lombok.RequiredArgsConstructor;
@@ -21,11 +23,9 @@ public class GlobalExceptionHandler {
 	private final MessageSource messageSource;
 
 	@ExceptionHandler(OptimisticLockingFailureException.class)
-	public void handleConflict(HttpServletRequest request, HttpServletResponse response, Exception ex)
-			throws IOException {
-		response.sendError(HttpServletResponse.SC_CONFLICT,
-				messageSource.getMessage("optimistic.locking.failure", null, LocaleContextHolder.getLocale()));
-		// see DefaultHandlerExceptionResolver
+	@ResponseStatus(value = HttpStatus.CONFLICT, reason = "optimistic.locking.failure")
+	public void handleConflict() {
+
 	}
 
 	@ExceptionHandler
