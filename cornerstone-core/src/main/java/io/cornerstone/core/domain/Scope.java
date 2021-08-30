fb52b5c9a9dc97382729776802ea9b134145095e
Yanming Zhou <zhouyanming@gmail.com>
2021-08-30 15:06:19 +0800

    Introduce Scope

---
+++ cornerstone-core/src/main/java/io/cornerstone/core/domain/Scope.java
@@ -0,0 +1,12 @@
+package io.cornerstone.core.domain;
+
+public enum Scope implements Displayable {
+	LOCAL, // only this jvm
+	APPLICATION, // all jvm for this application
+	GLOBAL; // all jvm for all application
+
+	@Override
+	public String toString() {
+		return getDisplayName();
+	}
+}
