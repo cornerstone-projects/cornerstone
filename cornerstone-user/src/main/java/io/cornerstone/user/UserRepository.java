db6618b0418cf0e8698d2e87efb502a8b577b806
Yanming Zhou <zhouyanming@gmail.com>
2021-09-02 11:16:37 +0800

    Annotate @Transactional on repository

--- cornerstone-user/src/main/java/io/cornerstone/user/UserRepository.java
+++ cornerstone-user/src/main/java/io/cornerstone/user/UserRepository.java
@@ -6,24 +6,29 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.transaction.annotation.Transactional;
 
 import io.cornerstone.core.repository.StreamableJpaRepository;
 
+@Transactional(readOnly = true)
 public interface UserRepository extends StreamableJpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
 	String CACHE_NAME = "user";
 
 	@Override
+	@Transactional
 	@Caching(evict = { @CacheEvict(cacheNames = CACHE_NAME, key = "#user.id"),
 			@CacheEvict(cacheNames = CACHE_NAME, key = "#user.username") })
 	<S extends User> S save(S user);
 
 	@Override
+	@Transactional
 	@Caching(evict = { @CacheEvict(cacheNames = CACHE_NAME, key = "#user.id"),
 			@CacheEvict(cacheNames = CACHE_NAME, key = "#user.username") })
 	<S extends User> S saveAndFlush(S user);
 
 	@Override
+	@Transactional
 	@Caching(evict = { @CacheEvict(cacheNames = CACHE_NAME, key = "#user.id"),
 			@CacheEvict(cacheNames = CACHE_NAME, key = "#user.username") })
 	void delete(User user);
--- showcase/src/main/java/io/example/showcase/customer/CustomerRepository.java
+++ showcase/src/main/java/io/example/showcase/customer/CustomerRepository.java
@@ -2,7 +2,9 @@ package io.example.showcase.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
 public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
 
 	boolean existsByIdNo(String idNo);
