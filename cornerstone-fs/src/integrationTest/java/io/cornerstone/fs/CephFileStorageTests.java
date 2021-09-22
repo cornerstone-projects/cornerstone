9641f416f5225865071ef857e9a36b4d40aee6c7
Yanming Zhou <zhouyanming@gmail.com>
2021-09-22 15:17:29 +0800

    Use GenericContainer instead of CephContainer

--- cornerstone-fs/src/integrationTest/java/io/cornerstone/fs/CephFileStorageTests.java
+++ cornerstone-fs/src/integrationTest/java/io/cornerstone/fs/CephFileStorageTests.java
@@ -3,24 +3,31 @@ package io.cornerstone.fs;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import com.clevercloud.testcontainers.ceph.CephContainer;
+import io.cornerstone.core.util.CodecUtils;
 
 @TestPropertySource(properties = { "file-storage.s3.bucket=test" })
 @Testcontainers
 class CephFileStorageTests extends FileStorageTestBase {
 
+	private static final String ACCESS_KEY = CodecUtils.nextId(32);
+	private static final String SECRET_KEY = CodecUtils.nextId(32);
+
 	@Container
-	static CephContainer container = new CephContainer();
+	static GenericContainer<?> container = new GenericContainer<>("quay.io/ceph/daemon")
+			.withEnv("CEPH_DEMO_ACCESS_KEY", ACCESS_KEY).withEnv("CEPH_DEMO_SECRET_KEY", SECRET_KEY)
+			.withEnv("RGW_NAME", "localhost").withEnv("CEPH_DEMO_UID", "admin").withEnv("NETWORK_AUTO_DETECT", "1")
+			.withExposedPorts(8080).withCommand("demo");
 
 	@DynamicPropertySource
 	static void registerDynamicProperties(DynamicPropertyRegistry registry) {
 		registry.add("file-storage.s3.endpoint",
-				() -> container.getRGWProtocol() + "://" + container.getRGWHTTPHostAddress());
-		registry.add("file-storage.s3.access-key", container::getRGWAccessKey);
-		registry.add("file-storage.s3.secret-key", container::getRGWSecretKey);
+				() -> "http://" + container.getHost() + ":" + container.getFirstMappedPort());
+		registry.add("file-storage.s3.access-key", () -> ACCESS_KEY);
+		registry.add("file-storage.s3.secret-key", () -> SECRET_KEY);
 	}
 
 }
