3c52d6a0ca3bc2bec8fb51d5d99f837301703201
Yanming Zhou <zhouyanming@gmail.com>
2022-04-18 10:21:38 +0800

    Switch from RedisIndexedSessionRepository to RedisSessionRepository
    For better performance

--- cornerstone-core/src/main/java/io/cornerstone/core/session/SessionConfiguration.java
+++ cornerstone-core/src/main/java/io/cornerstone/core/session/SessionConfiguration.java
@@ -1,18 +1,54 @@
 package io.cornerstone.core.session;
 
+import java.time.Duration;
+
 import io.cornerstone.core.redis.serializer.CompactJdkSerializationRedisSerializer;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.session.RedisSessionProperties;
+import org.springframework.boot.autoconfigure.session.SessionProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.serializer.support.SerializationFailedException;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
-import org.springframework.session.data.redis.RedisIndexedSessionRepository;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.session.data.redis.RedisSessionRepository;
 
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnClass(RedisIndexedSessionRepository.class)
+@ConditionalOnClass(RedisSessionRepository.class)
+@ConditionalOnBean(RedisConnectionFactory.class)
+@EnableConfigurationProperties(RedisSessionProperties.class)
+@EnableSpringHttpSession
 public class SessionConfiguration {
+
+	@Bean
+	public RedisSessionRepository sessionRepository(SessionProperties sessionProperties,
+			RedisSessionProperties redisSessionProperties, RedisConnectionFactory redisConnectionFactory,
+			RedisSerializer<?> springSessionDefaultRedisSerializer) {
+
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
+		redisTemplate.setKeySerializer(RedisSerializer.string());
+		redisTemplate.setHashKeySerializer(RedisSerializer.string());
+		redisTemplate.setValueSerializer(springSessionDefaultRedisSerializer);
+		redisTemplate.setHashValueSerializer(springSessionDefaultRedisSerializer);
+		redisTemplate.afterPropertiesSet();
+
+		RedisSessionRepository sessionRepository = new RedisSessionRepository(redisTemplate);
+		Duration timeout = sessionProperties.getTimeout();
+		if (timeout != null) {
+			sessionRepository.setDefaultMaxInactiveInterval(timeout);
+		}
+		sessionRepository.setRedisKeyNamespace(redisSessionProperties.getNamespace());
+		sessionRepository.setFlushMode(redisSessionProperties.getFlushMode());
+		sessionRepository.setSaveMode(redisSessionProperties.getSaveMode());
+		return sessionRepository;
+	}
 
 	@Bean
 	public RedisSerializer<?> springSessionDefaultRedisSerializer() {
