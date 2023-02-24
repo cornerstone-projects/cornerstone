0fce1ab4756ebd3a7848f7af4751b7a483db40f9
Yanming Zhou <zhouyanming@gmail.com>
2023-02-24 09:16:52 +0800

    Handler overflow properly

--- cornerstone-core/src/integrationTest/java/io/cornerstone/core/sequence/RedisSequenceTests.java
+++ cornerstone-core/src/integrationTest/java/io/cornerstone/core/sequence/RedisSequenceTests.java
@@ -1,15 +1,63 @@
 package io.cornerstone.core.sequence;
 
+import java.time.LocalDateTime;
+import java.time.Period;
+
 import io.cornerstone.core.sequence.CyclicSequence.CycleType;
 import io.cornerstone.core.sequence.cyclic.RedisCyclicSequence;
 import io.cornerstone.core.sequence.simple.RedisSimpleSequence;
 import io.cornerstone.test.containers.Redis;
+import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ContextConfiguration(classes = { RedisSequenceTests.Config.class, Redis.class })
 class RedisSequenceTests extends SequenceTestBase {
+
+	@Autowired
+	private RedisCyclicSequence sample3Sequence;
+
+	@Autowired
+	private BoundValueOperations<String, String> sample3SequenceOperations;
+
+	@Test
+	void testCrossCycle() {
+		LocalDateTime now = LocalDateTime.now();
+		LocalDateTime yesterday = now.minus(Period.ofDays(1));
+		String currentCycle = this.sample3Sequence.getCycleType().format(now);
+		String lastCycle = this.sample3Sequence.getCycleType().format(yesterday);
+		this.sample3SequenceOperations.set(lastCycle + "9998");
+		assertThat(this.sample3Sequence.nextStringValue()).isEqualTo(currentCycle + "0001");
+
+		this.sample3SequenceOperations.set(lastCycle + "9999");
+		assertThat(this.sample3Sequence.nextStringValue()).isEqualTo(currentCycle + "0001");
+	}
+
+	@Test
+	void testOverflowToNextCycle() {
+		LocalDateTime now = LocalDateTime.now();
+		LocalDateTime tomorrow = now.plus(Period.ofDays(1));
+		String currentCycle = this.sample3Sequence.getCycleType().format(now);
+		String nextCycle = this.sample3Sequence.getCycleType().format(tomorrow);
+		this.sample3SequenceOperations.set(currentCycle + "9999");
+		assertThat(this.sample3Sequence.nextStringValue()).isEqualTo(currentCycle + "10000");
+		assertThat(this.sample3SequenceOperations.get()).isEqualTo(nextCycle + "0000");
+		assertThat(this.sample3Sequence.nextStringValue()).isEqualTo(currentCycle + "10001");
+		assertThat(this.sample3SequenceOperations.get()).isEqualTo(nextCycle + "0001");
+	}
+
+	@Test
+	void testOverflowToInvalidDate() {
+		// 20230229 is invalid date
+		this.sample3SequenceOperations.set("202302290001");
+		this.sample3Sequence.nextStringValue();
+	}
 
 	static class Config {
 
@@ -24,6 +72,20 @@ class RedisSequenceTests extends SequenceTestBase {
 			cs.setCycleType(CycleType.MINUTE);
 			cs.setPaddingLength(7);
 			return cs;
+		}
+
+		@Bean
+		RedisCyclicSequence sample3Sequence() {
+			RedisCyclicSequence cs = new RedisCyclicSequence();
+			cs.setCycleType(CycleType.DAY);
+			cs.setPaddingLength(4);
+			return cs;
+		}
+
+		@Bean
+		BoundValueOperations<String, String> sample3SequenceOperations(StringRedisTemplate redisTemplate,
+				RedisCyclicSequence sample3Sequence) {
+			return redisTemplate.boundValueOps(RedisCyclicSequence.KEY_SEQUENCE + sample3Sequence.getSequenceName());
 		}
 
 	}
