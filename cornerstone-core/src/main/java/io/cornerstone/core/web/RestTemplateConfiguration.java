e9043b388d264ac74754eb502bef38f6f96b08c2
Yanming Zhou <zhouyanming@gmail.com>
2023-04-11 11:34:11 +0800

    Disable output streaming for SimpleClientHttpRequestFactory
    JDK HttpURLConnection does not handle 401 properly
    
    avoid java.net.HttpRetryException: cannot retry due to server
    authentication, in streaming mode

--- cornerstone-core/src/main/java/io/cornerstone/core/web/RestTemplateConfiguration.java
+++ cornerstone-core/src/main/java/io/cornerstone/core/web/RestTemplateConfiguration.java
@@ -5,8 +5,10 @@ import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConfigurer;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 
 @Configuration(proxyBeanMethods = false)
 public class RestTemplateConfiguration {
@@ -22,6 +24,15 @@ public class RestTemplateConfiguration {
 		RestTemplateBuilder builder = new RestTemplateBuilder().setConnectTimeout(this.connectTimeout)
 			.setReadTimeout(this.readTimeout);
 		return restTemplateBuilderConfigurer.configure(builder);
+	}
+
+	@Bean
+	RestTemplateCustomizer restTemplateCustomizer() {
+		return restTemplate -> {
+			if (restTemplate.getRequestFactory() instanceof SimpleClientHttpRequestFactory requestFactory) {
+				requestFactory.setOutputStreaming(false);
+			}
+		};
 	}
 
 }
