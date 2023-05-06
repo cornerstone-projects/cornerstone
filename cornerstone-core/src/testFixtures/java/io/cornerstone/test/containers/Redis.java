d7829f3fbcfbebd0c82d7994f8a71b0883814dd3
Yanming Zhou <zhouyanming@gmail.com>
2023-05-06 18:14:10 +0800

    Fix redis container is not actual used

--- cornerstone-core/src/testFixtures/java/io/cornerstone/test/containers/Redis.java
+++ cornerstone-core/src/testFixtures/java/io/cornerstone/test/containers/Redis.java
@@ -1,9 +1,14 @@
 package io.cornerstone.test.containers;
 
 import io.cornerstone.core.redis.DefaultRedisConfiguration;
+import io.cornerstone.core.redis.DefaultRedisConfiguration.DefaultRedisProperties;
+import org.testcontainers.containers.GenericContainer;
 
+import org.springframework.boot.context.properties.PropertyMapper;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 
 @TestConfiguration(proxyBeanMethods = false)
 @Import(DefaultRedisConfiguration.class)
@@ -12,6 +17,16 @@ public class Redis extends AbstractContainer {
 	@Override
 	protected int getExposedPort() {
 		return 6379;
+	}
+
+	@Primary
+	@Bean
+	public DefaultRedisProperties defaultRedisProperties(GenericContainer<?> redisContainer) {
+		DefaultRedisProperties properties = new DefaultRedisProperties();
+		PropertyMapper map = PropertyMapper.get();
+		map.from(redisContainer::getHost).to(properties::setHost);
+		map.from(redisContainer::getFirstMappedPort).to(properties::setPort);
+		return properties;
 	}
 
 }
