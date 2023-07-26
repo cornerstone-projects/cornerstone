7a84490c162aa56d06f3d8f05176b11091b83642
Yanming Zhou <zhouyanming@gmail.com>
2023-07-26 11:48:19 +0800

    Remove workaround for ThreadPoolTaskScheduler
    RedisHttpSessionConfiguration doesn't define SchedulingConfigurer since
    spring-session 3.0

--- cornerstone-core/src/main/java/io/cornerstone/core/task/TaskConfiguration.java
+++ cornerstone-core/src/main/java/io/cornerstone/core/task/TaskConfiguration.java
@@ -6,21 +6,13 @@ import io.github.resilience4j.bulkhead.BulkheadFullException;
 import io.github.resilience4j.ratelimiter.RequestNotPermitted;
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
-import org.springframework.boot.task.TaskExecutorBuilder;
-import org.springframework.boot.task.TaskSchedulerBuilder;
 import org.springframework.boot.task.TaskSchedulerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.task.TaskDecorator;
-import org.springframework.scheduling.annotation.AsyncAnnotationBeanPostProcessor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.scheduling.config.TaskManagementConfigUtils;
 
 @Slf4j
 @Order(0)
@@ -32,23 +24,6 @@ public class TaskConfiguration {
 	@Bean
 	TaskDecorator taskDecorator() {
 		return RunnableWithRequestId::new;
-	}
-
-	@Bean
-	@ConditionalOnBean(name = TaskManagementConfigUtils.SCHEDULED_ANNOTATION_PROCESSOR_BEAN_NAME)
-	public ThreadPoolTaskScheduler taskScheduler(TaskSchedulerBuilder builder) {
-		// RedisHttpSessionConfiguration.SessionCleanupConfiguration
-		// implements SchedulingConfigurer
-		// cause not created by TaskSchedulingAutoConfiguration
-		return builder.build();
-	}
-
-	@Bean(name = { TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME,
-			AsyncAnnotationBeanPostProcessor.DEFAULT_TASK_EXECUTOR_BEAN_NAME })
-	public ThreadPoolTaskExecutor taskExecutor(TaskExecutorBuilder builder) {
-		// taskScheduler implements Executor
-		// cause not created by TaskExecutionAutoConfiguration
-		return builder.build();
 	}
 
 	@Bean
