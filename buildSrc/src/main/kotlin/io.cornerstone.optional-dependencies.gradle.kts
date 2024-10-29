e34909fb5e3e124ac8c392d04581cfc56dbb91d9
Yanming Zhou <zhouyanming@gmail.com>
2024-10-29 09:06:20 +0800

    Simplify gradle pre-compiled script plugin

--- buildSrc/src/main/kotlin/io.cornerstone.optional-dependencies.gradle.kts
+++ buildSrc/src/main/kotlin/io.cornerstone.optional-dependencies.gradle.kts
@@ -1,20 +1,11 @@
-import org.gradle.api.Plugin
-import org.gradle.api.Project
-
-abstract class OptionalDependenciesPlugin : Plugin<Project> {
-	override fun apply(project: Project) {
-		project.configurations.create("optional").apply {
-			isCanBeConsumed = false
-			isCanBeResolved = false
-			val optional = this
-			project.plugins.withType<JavaPlugin> {
-				project.extensions.getByType<JavaPluginExtension>().sourceSets.forEach {
-					project.configurations.getByName(it.compileClasspathConfigurationName).extendsFrom(optional)
-					project.configurations.getByName(it.runtimeClasspathConfigurationName).extendsFrom(optional)
-				}
-			}
+configurations.create("optional").apply {
+	isCanBeConsumed = false
+	isCanBeResolved = false
+	val optional = this
+	plugins.withType<JavaPlugin> {
+		extensions.getByType<JavaPluginExtension>().sourceSets.forEach {
+			configurations.getByName(it.compileClasspathConfigurationName).extendsFrom(optional)
+			configurations.getByName(it.runtimeClasspathConfigurationName).extendsFrom(optional)
 		}
 	}
 }
-
-apply<OptionalDependenciesPlugin>()
