4e8df46b9ee17f0e8b99e5b6880df1242f31dea6
Yanming Zhou <zhouyanming@gmail.com>
2025-08-29 16:19:21 +0800

    Remove unnecessary @AutoConfigureAfter

--- cornerstone-core/src/main/java/io/cornerstone/core/CoreAutoConfiguration.java
+++ cornerstone-core/src/main/java/io/cornerstone/core/CoreAutoConfiguration.java
@@ -4,9 +4,7 @@ import io.cornerstone.core.persistence.convert.AbstractArrayConverter;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
 import org.springframework.boot.context.TypeExcludeFilter;
@@ -23,7 +21,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 		@Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class) })
 @EntityScan(basePackageClasses = AbstractArrayConverter.class)
 @AutoConfigureBefore(TaskExecutionAutoConfiguration.class)
-@AutoConfigureAfter(RedisAutoConfiguration.class)
 public class CoreAutoConfiguration {
 
 }
