b34f42e0ae41fbd1cd85b5c98b6a9e371ec5d15f
Yanming Zhou <zhouyanming@gmail.com>
2025-12-10 10:38:42 +0800

    Use `@WithMockUser` for WebMvcTestBase

--- cornerstone-core/src/testFixtures/java/io/cornerstone/test/WebMvcTestBase.java
+++ cornerstone-core/src/testFixtures/java/io/cornerstone/test/WebMvcTestBase.java
@@ -4,14 +4,17 @@ import io.cornerstone.core.web.DefaultWebMvcConfigurer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.TypeExcludeFilter;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.client.MockMvcClientHttpRequestFactory;
@@ -20,7 +23,7 @@ import org.springframework.web.client.RestTemplate;
 
 @WebMvcTest
 @ContextConfiguration
-@ImportAutoConfiguration(exclude = SecurityAutoConfiguration.class)
+@WithMockUser
 @ActiveProfiles("test")
 public abstract class WebMvcTestBase {
 
@@ -39,6 +42,11 @@ public abstract class WebMvcTestBase {
 			excludeFilters = { @Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
 					@Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class) })
 	static class Config {
+
+		@Bean
+		SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+			return http.csrf(CsrfConfigurer::disable).build();
+		}
 
 	}
 
