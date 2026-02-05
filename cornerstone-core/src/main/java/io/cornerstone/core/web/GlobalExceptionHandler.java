75664ec07e8a007f7a186e4f200ba85487eeaa83
Yanming Zhou <zhouyanming@gmail.com>
2026-02-05 15:57:51 +0800

    Handle PropertyReferenceException to produce client error

--- cornerstone-core/src/main/java/io/cornerstone/core/web/GlobalExceptionHandler.java
+++ cornerstone-core/src/main/java/io/cornerstone/core/web/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.data.core.PropertyReferenceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -28,10 +29,16 @@ public class GlobalExceptionHandler {
 	}
 
 	@ExceptionHandler
-	public void handleMaxUploadSizeExceeded(HttpServletRequest request, HttpServletResponse response,
+	public void handleMaxUploadSizeExceededException(HttpServletRequest request, HttpServletResponse response,
 			MaxUploadSizeExceededException ex) throws IOException {
 		response.sendError(HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE, this.messageSource.getMessage(
 				"max.upload.size.exceeded", new Object[] { ex.getMaxUploadSize() }, LocaleContextHolder.getLocale()));
+	}
+
+	@ExceptionHandler
+	public void handlePropertyReferenceException(HttpServletRequest request, HttpServletResponse response,
+			PropertyReferenceException ex) throws IOException {
+		response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
 	}
 
 }
